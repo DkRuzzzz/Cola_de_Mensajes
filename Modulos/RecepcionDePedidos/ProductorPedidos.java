d4a53e328df1672d3b2a52596f7e804a38ce2d53
DkRuzzzz <jesus5gen@gmail.com>
2026-03-04 10:50:52 -0600

    Add files via upload

---
+++ Modulos/RecepcionDePedidos/ProductorPedidos.java
@@ -0,0 +1,33 @@
+import javax.jms.*;
+import com.sun.messaging.ConnectionFactory;
+import com.sun.messaging.ConnectionConfiguration;
+
+public class ProductorPedidos {
+
+    public static void main(String[] args) {
+        try {
+
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.setProperty(ConnectionConfiguration.imqAddressList, "localhost:7676");
+
+            Connection connection = factory.createConnection();
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+            Queue queue = session.createQueue("pedidos");
+            MessageProducer producer = session.createProducer(queue);
+
+            TextMessage message = session.createTextMessage(
+                    "{\"pedidoId\":1,\"producto\":\"Laptop\",\"cliente\":\"Jesus\"}"
+            );
+
+            producer.send(message);
+
+            System.out.println("Mensaje enviado correctamente.");
+
+            connection.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
